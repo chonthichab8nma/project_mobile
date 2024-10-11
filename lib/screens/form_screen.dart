abf1e8e2c1824adab69f9559bea5e58c2b2453f0
chonthichab8nma <chonthichaboo2@gmail.com>
2024-10-11 22:08:46 +0700

    edit

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -2,6 +2,7 @@ import 'package:account/main.dart';
 import 'package:account/models/transactions.dart';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:account/provider/transaction_provider.dart';
 
@@ -16,11 +17,25 @@ class _FormScreenState extends State<FormScreen> {
   final formKey = GlobalKey<FormState>();
 
   final idController = TextEditingController();
-  final titleController = TextEditingController();
+  final noteController = TextEditingController();
+
   String? selectedEmotionColor;
   DateTime currentDate = DateTime.now();
 
   final List<String> emotionColors = ['Red', 'Blue', 'Green', 'Yellow', 'Pink'];
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2024),
+        lastDate: DateTime(2024));
+    if (picked != null && picked != currentDate) {
+      setState(() {
+        currentDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,88 +46,109 @@ class _FormScreenState extends State<FormScreen> {
         body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
-                key: formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Card(
-                      elevation: 2.0,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'ไอดี (ใช้เพื่อบอกว่าสีอะไรเป็นสีอะไร)',
-                          ),
-                          controller: idController,
-                          validator: (String? str) {
-                            if (str!.isEmpty) {
-                              return 'กรุณากรอกไอดี';
-                            }
-                            return null;
-                          },
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    elevation: 2.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'ID',
                         ),
+                        controller: idController,
+                        validator: (String? str) {
+                          if (str!.isEmpty) {
+                            return 'ID Please';
+                          }
+                          return null;
+                        },
                       ),
                     ),
-                  ],
-                )
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    elevation: 2.0,
+                    child: ListTile(
+                        title: Text(
+                            'วันที่ : ${DateFormat('yyyy-MM-dd').format(currentDate)}'),
+                        trailing: const Icon(Icons.calendar_today),
+                        onTap: () => _selectDate(context)),
+                  ),
+                  const SizedBox(height: 20),
+                  Card(
+                    elevation: 2.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'Note'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
 
-                //children: [
-                //TextFormField(
-                //decoration: const InputDecoration(
-                //labelText: 'ID',
-                //),
-                //controller: titleController,
-                //validator: (String? str) {
-                //if (str!.isEmpty) {
-                //return 'กรุณากรอกไอดี';
-                //}
-                //return null;
-                //},
-                //),
-                //TextFormField(
-                //decoration: const InputDecoration(
-                //labelText: 'จำนวนเงิน',
-                //),
-                //keyboardType: TextInputType.number,
-                //controller: amountController,
-                //validator: (String? input) {
-                //try {
-                //double amount = double.parse(input!);
-                //if (amount < 0) {
-                //return 'กรุณากรอกข้อมูลมากกว่า 0';
-                //}
-                //} catch (e) {
-                //return 'กรุณากรอกข้อมูลเป็นตัวเลข';
-                //}
-                //},
-                //),
-                //TextButton(
-                //  child: const Text('บันทึก'),
-                //onPressed: () {
-                //if (formKey.currentState!.validate()) {
-                // create transaction data object
-                //var statement = Transactions(
-                //  keyID: null,
-                //title: titleController.text,
-                //amount: double.parse(amountController.text),
-                //date: DateTime.now());
+              //children: [
+              //TextFormField(
+              //decoration: const InputDecoration(
+              //labelText: 'ID',
+              //),
+              //controller: titleController,
+              //validator: (String? str) {
+              //if (str!.isEmpty) {
+              //return 'กรุณากรอกไอดี';
+              //}
+              //return null;
+              //},
+              //),
+              //TextFormField(
+              //decoration: const InputDecoration(
+              //labelText: 'จำนวนเงิน',
+              //),
+              //keyboardType: TextInputType.number,
+              //controller: amountController,
+              //validator: (String? input) {
+              //try {
+              //double amount = double.parse(input!);
+              //if (amount < 0) {
+              //return 'กรุณากรอกข้อมูลมากกว่า 0';
+              //}
+              //} catch (e) {
+              //return 'กรุณากรอกข้อมูลเป็นตัวเลข';
+              //}
+              //},
+              //),
+              //TextButton(
+              //  child: const Text('บันทึก'),
+              //onPressed: () {
+              //if (formKey.currentState!.validate()) {
+              // create transaction data object
+              //var statement = Transactions(
+              //  keyID: null,
+              //title: titleController.text,
+              //amount: double.parse(amountController.text),
+              //date: DateTime.now());
 
-                // add transaction data object to provider
-                //                       var provider = Provider.of<TransactionProvider>(context,
-                //                         listen: false);
+              // add transaction data object to provider
+              //                       var provider = Provider.of<TransactionProvider>(context,
+              //                         listen: false);
 //
-                //                      provider.addTransaction(statement);
+              //                      provider.addTransaction(statement);
 //
-                //                      Navigator.push(
-                //                        context,
-                //                      MaterialPageRoute(
-                //                        fullscreenDialog: true,
-                //                      builder: (context) {
-                //                      return MyHomePage();
-                //                  }));
-                //      }
-                //  })
-                // ],
-                )));
+              //                      Navigator.push(
+              //                        context,
+              //                      MaterialPageRoute(
+              //                        fullscreenDialog: true,
+              //                      builder: (context) {
+              //                      return MyHomePage();
+              //                  }));
+              //      }
+              //  })
+              // ],
+            )));
   }
 }
